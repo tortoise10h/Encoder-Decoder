23f9c1e9c8726a4afaaf8bb70b5064ffbbabd5a0
Nguyen Tan Huy <tortoise10h@gmail.com>
2020-03-28 23:14:47 +0700

    Shift Cipher & Affine

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace encoder_decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("#Yo, Welcome to Encoder_Decoder console#");
+                Console.WriteLine("1. Encode using Shift Cipher.");
+                Console.WriteLine("2. Decode of Shift Cipher.");
+                Console.WriteLine("3. Encode using Affine.");
+                Console.WriteLine("4. Decode of Affine.");
+                Console.WriteLine("0. Exit!");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 0)
+                {
+                    break;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter string to encode: ");
+                            string code = Console.ReadLine();
+                            Console.WriteLine("Enter k: ");
+                            int k = Convert.ToInt32(Console.ReadLine());
+                            string encoded = ShiftCipher.encode(code, k);
+                            Console.WriteLine("\n>>> Encoded of '{0}' is: {1}\n", code, encoded);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Enter string to decode: ");
+                            string code = Console.ReadLine();
+                            Console.WriteLine("Enter k: ");
+                            int k = Convert.ToInt32(Console.ReadLine());
+                            string decoded = ShiftCipher.decode(code, k);
+                            Console.WriteLine("\n>>> Decoded of '{0}' is: {1}\n", code, decoded);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter string to encode: ");
+                            string code = Console.ReadLine();
+                            Console.WriteLine("Enter key: ");
+                            string key = Console.ReadLine();
+                            string encoded = Affine.encode(code, key);
+                            Console.WriteLine("\n>>> Encoded of '{0}' is: {1}\n", code, encoded);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter string to decode: ");
+                            string code = Console.ReadLine();
+                            Console.WriteLine("Enter key: ");
+                            string key = Console.ReadLine();
+                            string decoded = Affine.decode(code, key);
+                            Console.WriteLine("\n>>> Encode of '{0}' is: {1}\n", code, decoded);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("!!!Wrong number! Please enter again!!!\n");
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine("\n\n###################################\n#####!!!Thank for using <3 !!!#####\n###################################");
+        }
+    }
+}
---
+++ ShiftCipher.cs
@@ -0,0 +1,72 @@
+using System;
+
+namespace encoder_decoder
+{
+    public class ShiftCipher
+    {
+        private static char[] characters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public static string encode(string code, int k)
+        {
+
+            // split code to array single character of code
+            char[] codeArr = code.ToCharArray();
+            try
+            {
+                if (code.Equals("") == true)
+                {
+                    throw new System.ArgumentException("Please enter non empty string", "original");
+                }
+                if (k > 26 || k < -26)
+                {
+                    throw new System.ArgumentException("k range is only from -26 -> 26", "original");
+                }
+            }
+            catch (System.ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            string result = "";
+
+            foreach (char c in codeArr)
+            {
+                int charCurrentPositionInAlphabet = Array.IndexOf(characters, c);
+                int delta = charCurrentPositionInAlphabet + k;
+                int newCharPositionInAlphabet = HandleNewCharPositionWhenDecode(delta);
+
+                result += characters[newCharPositionInAlphabet];
+            }
+
+            return result;
+        }
+
+        private static int HandleNewCharPositionWhenDecode(int delta)
+        {
+            int newCharPositionInAlphabet = 0;
+            if (delta > 0 && delta <= characters.Length)
+            {
+                // shift forward and not repeat the cycle
+                newCharPositionInAlphabet = delta;
+
+            }
+            else if (delta > 0 && delta > characters.Length)
+            {
+                // shift forward and repeat the cycle
+                newCharPositionInAlphabet = delta - characters.Length;
+            }
+            else if (delta < 0 && Math.Abs(delta) <= characters.Length)
+            {
+                // shift backward
+                newCharPositionInAlphabet = delta + characters.Length;
+            }
+
+            return newCharPositionInAlphabet;
+        }
+
+
+        public static string decode(string code, int k)
+        {
+            return encode(code, -k);
+        }
+    }
+}
